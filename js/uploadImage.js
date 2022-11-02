8e4e66caf0c561d9b1c3d31adba4f75b2a56603a
Mukesh Mahato <mukeshmahato843@gmail.com>
2022-11-03 00:10:54 +0545

    change profile picture js

---
+++ js/uploadImage.js
@@ -0,0 +1,42 @@
+let imageInput = document.getElementById("uploadImage");
+const img = document.querySelectorAll('#avtar');
+const profileForm = document.getElementById("imageUploadForm");
+const profilePicError = document.getElementById("pp-error");
+
+imageInput.addEventListener("change", function () {
+    const choosedFile = this.files[0];
+    if (choosedFile) {
+        const reader = new FileReader(); //FileReader is a predefined function of JS
+        reader.addEventListener('load', function () {
+
+            const url = "http://localhost/minor%20project/php/uploadImage.php";
+            const xhr = new XMLHttpRequest();
+
+            xhr.open("POST", url, true);
+
+            xhr.onprogress = () => {
+                console.log("progress");
+                // loading animation will be added
+            }
+
+            xhr.onload = () => {
+                if (xhr.responseText === "OK") {
+                    profilePicError.style.visibility = "hidden";
+                    img[1].style = "border:2px solid; border-color:#00c300;";
+                } else {
+                    profilePicError.innerHTML = xhr.responseText;
+                    img[1].style = "border-color:red";
+                    profilePicError.style.visibility = "visible";
+                }
+            }
+
+            let dataToSend = new FormData(document.getElementById("imageUploadForm"))
+            xhr.send(dataToSend);
+
+            img[1].setAttribute('src', reader.result);      // [1] because we have 2 images with id avtar,
+        });
+        reader.readAsDataURL(choosedFile);
+    } else {
+        img.setAttribute('src', "./img/Mukesh Mahato Facebook PP.jpeg");
+    }
+})
